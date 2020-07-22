027e366e3d7efedcc4b8e8e85aca1c1d904ea0b9
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-07-22 22:25:00 +0200

    Add OptionResolver Class

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
